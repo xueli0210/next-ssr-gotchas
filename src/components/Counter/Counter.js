de3a5eaedea5d45fc1b0f4ec34c9888991eaf73e
Xue Li <xuelimiis@gmail.com>
2024-09-24 12:12:18 -0700

    [bugfix] fix an error of SSR not having access of window object

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -2,12 +2,11 @@
 import React from 'react';
 
 function Counter() {
-  const [count, setCount] = React.useState(() => {
-    return Number(
-      window.localStorage.getItem('saved-count') || 0
-    );
-  });
+  const [count, setCount] = React.useState(0);
 
+  React.useEffect(() => {
+    setCount(Number(window.localStorage.getItem("saved-count") || 0));
+  }, [])
   React.useEffect(() => {
     window.localStorage.setItem('saved-count', count);
   }, [count]);
